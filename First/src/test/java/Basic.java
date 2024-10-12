692b4db6ebe51ecb1759b94eb241c523c502c7bd
rohitlodhi <rohitrajpoot692@gmail.com>
2024-10-12 23:10:35 +0530

    Add basic file

--- First/src/test/java/Basic.java
+++
@@ -1,7 +0,0 @@
-
-public class Basic {
-
-	public static void main(String[] args) {
-		System.out.println("run");
-	}
-}
---
+++ Second/src/test/java/JenkinsSecond/Trial2.java
@@ -0,0 +1,8 @@
+package JenkinsSecond;
+
+public class Trial2 {
+
+	public static void main(String[] args) {
+		System.out.println("RUN2");
+	}
+}
